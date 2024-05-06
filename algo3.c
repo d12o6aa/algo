4a92016b12d99d7842c4cb805525b1de42193096
d12o6aa <doaakarem798@gmail.com>
2024-05-06 18:39:55 +0300

    update v3

--- algo3.c
+++ algo3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int merge(int a[], int x, int b[], int y, int k) {
+int kthElement2(int a[], int x, int b[], int y, int k) {
     if (x == 0) {
         return b[k - 1];
     }
@@ -10,15 +10,12 @@ int merge(int a[], int x, int b[], int y, int k) {
     }
 
     if (a[0] < b[0]) {
-        return (k == 1) ? a[0] : merge(a + 1, x - 1, b, y, k - 1);
+        return (k == 1) ? a[0] : kthElement2(a + 1, x - 1, b, y, k - 1);
     } else {
-        return (k == 1) ? b[0] : merge(a, x, b + 1, y - 1, k - 1);
+        return (k == 1) ? b[0] : kthElement2(a, x, b + 1, y - 1, k - 1);
     }
 }
 
-int kthElement2(int a[], int x, int b[], int y, int k) {
-    return merge(a, x, b, y, k);
-}
 
 int main() {
     int arr1[] = { 100, 112 ,256 ,349 ,770};
